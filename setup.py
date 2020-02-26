af9af7ca8e5ee42d0ae37d8a95608336a69f3e5a
TralahM <musyoki.tralah@students.jkuat.ac.ke>
2020-02-26 23:13:44 +0300

    using django setting module instead

--- setup.py
+++ setup.py
@@ -3,17 +3,18 @@ from setuptools import setup, find_packages
 import firebase_orm
 
 setup(
-    name='firebase_orm',
+    name='django-firebase-orm',
     version=firebase_orm.__version__,
-    description='NoSQL object model database',
-    author='Vladimir Filipenko',
-    author_email='zavx0z@ya.ru',
-    url='https://github.com/zavx0z/firebase_orm',
+    description='NoSQL object model database for django ORM integration',
+    author='Tralah M Brian',
+    author_email='musyoki.brian@tralahtek.com',
+    url='https://github.com/TralahM/firebase_orm',
     packages=find_packages(),
     long_description=open('README.rst').read(),
     install_requires=[
         'firebase-admin>=2.9.0',
-        'grpcio>=1.9.1'
+        'grpcio>=1.9.1',
+        'django',
     ],
     test_suite='tests',
     license='MIT',
