b84edcc6f6a464529eb6515fe80186d12257eb58
TralahM <musyoki.tralah@students.jkuat.ac.ke>
2020-02-29 03:43:11 +0300

    more field definitions

--- firebase_orm/models/fields/__init__.py
+++ firebase_orm/models/fields/__init__.py
@@ -2,7 +2,7 @@ from firebase_orm.exceptions import CanNotBeChanged
 
 
 class Field:
-    def __init__(self, db_column=None):
+    def __init__(self, db_column=None,*args,**kwargs):
         self.db_column = db_column
 
     def __get__(self, obj, objtype):
@@ -29,4 +29,38 @@ class AutoField:
     def __set__(self, obj, val):
         raise CanNotBeChanged
 
+class CharField(Field):
+    pass
 
+class DateTimeField:
+    pass
+
+class DateField:
+    pass
+
+class TimeField:
+    pass
+
+class ForeignKey:
+    pass
+
+class ManyToManyField:
+    pass
+
+class OneToOneField:
+    pass
+
+class UUIDField:
+    pass
+
+class PhonenumberField(CharField):
+    pass
+
+class DecimalField(Field):
+    pass
+
+class IntegerField(Field):
+    pass
+
+class FloatField(Field):
+    pass
